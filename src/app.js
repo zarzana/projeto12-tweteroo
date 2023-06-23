065772869f97d84b485bb2f282a88d908e3fc5fa
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-06-23 13:27:49 -0300

    Added GET and POSTs.

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let user;
+let tweets = [];
+
+app.post('/sign-up', (req, res) => {
+    user = req.body;
+    res.send('OK');
+});
+
+app.post('/tweets', (req, res) => {
+    if (!user) { res.send('“UNAUTHORIZED”') };
+    tweets.push(req.body);
+    res.send('OK');
+});
+
+app.get('/tweets', (req, res) => {
+    tweetsToSend = tweets.slice(-10);
+    if (tweetsToSend.lenght > 0) { tweetsToSend.forEach(tweet => { tweet.push(user.avatar) }) };
+    res.send(tweetsToSend);
+});
+
+const port = 4000;
+app.listen(port, () => console.log(`Running server on port ${port}`));
