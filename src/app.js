8aeba9feaf1466df4e3150cf3f85f8f42e9e1bb3
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-06-23 17:10:41 -0300

    Fixed persistency issue.

--- src/app.js
+++ src/app.js
@@ -11,25 +11,35 @@ let tweets = [];
 app.post('/sign-up', (req, res) => {
     user = req.body;
     res.send('OK');
+    return;
 });
 
 app.post('/tweets', (req, res) => {
     if (!user) {
         res.status(401);
         res.send('UNAUTHORIZED');
-    };
+        return;
+    }
     tweets.push(req.body);
     res.status(200);
     res.send('OK');
+    return;
 });
 
 app.get('/tweets', (req, res) => {
-    let tweetsToSend = tweets.slice(-10);
-    if (tweetsToSend.lenght > 0) {
-        tweetsToSend.forEach(tweet => { tweet['avatar'] = user.avatar });
-    };
+    // let tweetsToSend = tweets.slice(-10);
+    // if (tweetsToSend.lenght > 0) {
+    //     tweetsToSend.forEach(tweet => { tweet['avatar'] = user.avatar });
+    // };
+    let tweetsToSend = [];
+    tweets.slice(-10).forEach(tweet => {
+        let currentTweet = tweet;
+        currentTweet['avatar'] = user.avatar;
+        tweetsToSend.push(currentTweet);
+    });
     res.status(200);
     res.send(tweetsToSend);
+    return;
 });
 
 const port = 5000;
